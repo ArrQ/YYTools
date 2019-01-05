f37b9782e11821231c19c3d323b7dcd4464f2033
ArrQ <wyytai@qq.com>
2019-01-05 17:21:59 +0800

    -[add]基础文件

---
+++ YYTools/YYTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYTools
+//
+//  Created by admin on 2019/1/5.
+//  Copyright © 2019年 ArrQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYTools/YYTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYTools
+//
+//  Created by admin on 2019/1/5.
+//  Copyright © 2019年 ArrQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
